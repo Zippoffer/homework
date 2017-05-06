de2a77238e53ba7cd791944b55f09048d25277bf
christopherhill <thorsdaddy77@gmail.com>
2017-05-06 14:02:51 -0500

    added image, cleanup

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
+
+
 'use strict'
 
-	  var display = document.getElementById("display");
-	  var inputBox = document.getElementById("inputBox");
-	  var outputBox = document.getElementById("outputBox");
+var display = document.getElementById("display");
+var inputBox = document.getElementById("inputBox");
+var outputBox = document.getElementById("outputBox");
 
 
-  function parse(){
-
-		var word = display.value
-
-	  outputBox.value = word.replace(/\•/g, '');
-
-  }
+function parse(){
+	var word = display.value
+  outputBox.value = word.replace(/\•/g, '');
+}
 
 inputBox.onkeyup = function(){
-	var x = "••••••"
+
+	var x = ' ••• •••••'
 	var o = inputBox.value
-
+	
     document.getElementById('display').innerHTML = x+x+x+x+x+x+x+x+x+x+x+x+x
-    +x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+o+x+x+x+x+x+x+x
-    +x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x
-    
-
+    +x+x+x+x+x+x+x+x+o+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x+x
+    +x+x+x+x+x+x
 }
